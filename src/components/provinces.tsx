beda6b8641d23f07e03f66ea00f976e23846312e
Nino Nakano <yudhaprasty17@gmail.com>
2023-11-12 22:45:38 +0700

    success

--- src/components/provinces.tsx
+++ src/components/provinces.tsx
@@ -1,25 +1,133 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import '../assets/css/dropdown.css'
-import { useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css' 
+import 'bootstrap/dist/js/bootstrap.min.js' 
+import '../assets/css/dropdown.css' 
+import { useEffect, useState } from 'react' 
+import { Button, Form } from 'react-bootstrap'; 
+ 
+interface provinces { 
+    id: number 
+    name: string 
+} 
+ 
+interface cities { 
+    id: number 
+    province_id: number 
+    name: string 
+} 
+ 
+interface regencies { 
+    id: number 
+    regency_id: number 
+    name: string 
+} 
 
-interface provinces{
-    id: string
+interface villages {
+    id: number
+    district_id: number
     name: string
 }
+ 
+export default function Provinces() { 
+    const [data, setData] = useState<provinces[]>() 
+    const [city, setCity] = useState<cities[]>() 
+    const [regencies, setRegencies] = useState<regencies[]>()
+    const [villages, setVillages] = useState<villages[]>() 
+    const getInitialState = () => { 
+        const value = "1" 
+        return value 
+    } 
+ 
+    const getInitialRegencyId = () => { 
+        const value = "1" 
+        return value 
+    } 
 
-export default function Provinces(){
-    const [ data, setData ] = useState<provinces[]>()
-    
-    useEffect(() => {
-        fetch('https://www.emsifa.com/api-wilayah-indonesia/api/provinces.json')
+    const getInitialVillageId = () => {
+        const value = "1"
+        return value
+    }
+ 
+ 
+    const [provinceId, setProvinceId] = useState(getInitialState) 
+    const [regencyId, setRegencyId] = useState(getInitialRegencyId)
+    const [villageId, setVillageId] = useState(getInitialVillageId) 
+ 
+    const handleChange = (event: any) => { 
+        setProvinceId(event.target.value) 
+    } 
+ 
+    const handleChangeRegency = (event: any) => { 
+        setRegencyId(event.target.value) 
+    } 
+
+    const handleChangeVillage = (event: any) => {
+        setVillageId(event.target.value)
+    }
+ 
+    useEffect(() => { 
+        fetch('https://www.emsifa.com/api-wilayah-indonesia/api/provinces.json') 
+            .then(res => res.json()) 
+            .then(res => { 
+                setData(res) 
+            }) 
+            .catch(res => console.log(res)) 
+ 
+    }, []) 
+ 
+    if(provinceId != "1") { 
+        const getDataCity = fetch(`https://www.emsifa.com/api-wilayah-indonesia/api/regencies/${provinceId}.json`) 
+        .then(res => res.json()) 
+        .then(res => { 
+            setCity(res) 
+            console.log(res) 
+        }) 
+        getDataCity 
+    } 
+ 
+    if(regencyId != "1") { 
+        const getRegency = fetch(`https://www.emsifa.com/api-wilayah-indonesia/api/districts/${regencyId}.json`) 
+        .then(res => res.json()) 
+        .then(res => { 
+            setRegencies(res) 
+        }) 
+        getRegency 
+    } 
+
+    if(villageId != "1") {
+        const getVillage = fetch(`https://www.emsifa.com/api-wilayah-indonesia/api/villages/${villageId}.json`)
         .then(res => res.json())
         .then(res => {
-            setData(res)
+            setVillages(res)
         })
-        .catch(res => console.log(res))
-    
-    }, [])
-        
-    return(data?.values)
+        getVillage
+    }
+ 
+    return (<> 
+        <Form.Select id='province' aria-label="Default select example" value={provinceId} onChange={handleChange}> 
+            <option value={`1`}>Pilih provinsi</option> 
+            {data?.map((data: any, index: any) => ( 
+                <option id='selected-province' className="dropdown-item" value={data.id}>{data.name}</option> 
+            ))} 
+        </Form.Select> 
+        <Form.Select id='city' aria-label="Default select example" value={regencyId} onChange={handleChangeRegency}> 
+            <option value={`1`}>Pilih Kota</option> 
+            {city?.map((city: any, index: any) => ( 
+                <option id='selected-province' className="dropdown-item" value={city.id}>{city.name}</option> 
+            ))} 
+        </Form.Select> 
+        <Form.Select id='disctrict' aria-label="Default select example" onChange={handleChangeVillage}> 
+            <option value={`1`}>Pilih Kecamatan</option> 
+            {regencies?.map((disctrict: any, index: any) => ( 
+                <option id='selected-province' className="dropdown-item" value={disctrict.id}>{disctrict.name}</option> 
+            ))} 
+        </Form.Select> 
+        <Form.Select id='village' aria-label="Default select example"> 
+            <option value={`1`}>Pilih Kelurahan</option> 
+            {villages?.map((villages: any, index: any) => ( 
+                <option id='selected-province' className="dropdown-item" value={villages.id}>{villages.name}</option> 
+            ))} 
+        </Form.Select> 
+    </> 
+ 
+    ) 
 }
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,22 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import '../assets/css/dropdown.css'
 import Provinces from "../components/provinces"
 
-
-const dataProvince = Provinces
 
 export default function Landing(){ 
     return(
         <>
-            <div className="dropdown">
-                <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style={{width: 500}}>
-                    Pilih Provinsi
-                </button>
-                <ul className="dropdown-menu">
-                    {dataProvince.map((data: any, index: any) => (
-                        <li style={{width: 481}}><option id='selected' className="dropdown-item" value={data.id}>{data.name}</option></li>
-                    ))
-                    }
-                </ul>
-            </div>
+            <Provinces />
         </>
     )
     
